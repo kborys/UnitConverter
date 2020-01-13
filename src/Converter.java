46297f4e463ad2fa2fb5c372c44910a098c9fd96
kborys <konrad.borys@outlook.com>
2020-01-13 12:37:07 +0100

    Code refactoring
    -Did some code refactoring
    -Deleted temporary file ConverterTest.java
    - Added subclasses to LenghtUnit. Now the Converter supports also conversion from milimeters, centimeters, decimeters, meters and kilometers.

--- src/Converter.java
+++ src/Converter.java
@@ -1,60 +1,129 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Converter {
 
     public static void main(String[] args){
-        boolean working = true;
-        Scanner sc = new Scanner(System.in);
+        ArrayList<String> unitTypes = new ArrayList<String>();
+        unitTypes.add("length");
+        unitTypes.add("weight");
+        unitTypes.add("temperature");
 
-        System.out.print("Witam w UnitConverter.");
-        while(working) {
-            String valueType;
-            System.out.println("Jeśli chcesz coś przekonwertować wpisz konwertowany typ jednostki z poniższej listy: ");
-            System.out.println("length.");
-            System.out.println("W przecim wypadku wpisz 'exit'.");
-            String i = sc.next();
-            System.out.println("Wprowadź liczbę, którą chcesz przekonwertować.");
-            double value = sc.nextDouble();
-            LengthUnit inputUnit;
-            double outputValue = 0;
-            boolean active = true;
+        ArrayList<String> lengthUnits = new ArrayList<String>();
+        lengthUnits.add("mm");
+        lengthUnits.add("cm");
+        lengthUnits.add("dm");
+        lengthUnits.add("m");
+        lengthUnits.add("km");
 
-            if (i.equals("length")) {
-                System.out.println("Wybrałeś konwersje długości.");
-                System.out.println("Wybierz jednostkę bazową: mm, cm, dm, m, km. W innym wypadku wpisz exit.");
-                i = sc.next();
+        LengthUnit converter = null;
 
-                if (i.equals("mm")) {
-                    inputUnit = new Milimeter();
-                    System.out.println("Wybrałeś milimetry.");
-                    System.out.println("Wybierz jednostkę docelową: mm, cm, dm, m, km. W innym wypadku wpisz exit.");
-                    i = sc.next();
+        //boolean working = true;
+        boolean correctUnitType = false;
+        boolean correctBaseUnit = false;
+        boolean correctTargetUnit = false;
+        boolean correctInputValue = false;
+        String inputUnitType = null;
+        String inputBaseUnit = null;
+        String inputTargetUnit = null;
+        double inputValue = 0;
+        double outputValue = 0;
+        Scanner scan = new Scanner(System.in);
 
-                    if (i.equals("mm")) {
-                        outputValue = inputUnit.toMilimeters(value);
-                        //System.out.println(value + " mm jest równe " + outputValue + " mm.");
-                    } else if (i.equals("cm")) {
-                        outputValue = inputUnit.toCentimeters(value);
-                        //System.out.println(value + " mm jest równe " + outputValue + " cm.");
-                    } else if (i.equals("dm")) {
-                        outputValue = inputUnit.toDecimeters(value);
-                        //System.out.println(value + " mm jest równe " + outputValue + " cm.");
-                    } else if (i.equals("m")) {
-                        outputValue = inputUnit.toMeters(value);
-                        //System.out.println(value + " mm jest równe " + outputValue + " cm.");
-                    } else if (i.equals("km")) {
-                        outputValue = inputUnit.toKilometers(value);
-                        //System.out.println(value + " mm jest równe " + outputValue + " cm.");
-                    } else {
-                        System.out.println("Taka jednostka nie istnieje. Spróbuj ponownie.");
-                    }
+        System.out.println("Welcome in the UnitConverter.");
 
-                    System.out.println(value + " mm jest równe " + outputValue + " " + i + ".");
+        //wprowadzenie typu jednostki sposrod dlugosci, temperatury czy wagi. petla powtarzana tak dlugo, az zostanie wprowadzona poprawna wartosc
+        while(!correctUnitType){
+            System.out.println("Choose desired unit type:");
+            System.out.println("length, new are coming soon.");
+
+            inputUnitType = scan.nextLine().toLowerCase();
+
+            if(unitTypes.contains(inputUnitType)) {
+                correctUnitType = true;
+                System.out.println("You chose " + inputUnitType + ".");
+            } else {
+                inputUnitType = null;
+                System.out.println("You chose wrong unit type. Try again.");
+            }
+        }
+
+        //jezeli wprowadzono juz typ jednostki to nastepuje wprowadzenie bazowej jednostki. petla powtarzana tak dlugo, az zostanie wprowadzona poprawna wartosc
+        while(correctUnitType && !correctBaseUnit){
+            System.out.println("Choose desired base unit:");
+            System.out.println("mm, cm, dm, m, km");
+
+            inputBaseUnit = scan.nextLine().toLowerCase();
+
+            if(lengthUnits.contains(inputBaseUnit)) {
+                correctBaseUnit = true;
+                System.out.println("You chose " + inputBaseUnit + ".");
+
+                if (inputBaseUnit.equals("mm")){
+                    converter = new Milimeter();
+                } else if(inputBaseUnit.equals("cm")){
+                    converter = new Centimeter();
+                } else if(inputBaseUnit.equals("cm")){
+                    converter = new Centimeter();
+                } else if(inputBaseUnit.equals("dm")){
+                    converter = new Decimeter();
+                } else if(inputBaseUnit.equals("m")){
+                    converter = new Meter();
+                } else if(inputBaseUnit.equals("km")){
+                    converter = new Kilometer();
+                } else {
+                    System.out.println("Something went wrong. Contact with the administrator");
                 }
+            } else {
+                inputBaseUnit = null;
+                System.out.println("You chose wrong base unit. Try again.");
+            }
+        }
+
+        //jezeli wprowadzono juz typ jednostki i bazowa jednostke to nastepuje wprowadzenie docelowej jednostki jednostki. petla powtarzana tak dlugo, az zostanie wprowadzona poprawna wartosc
+        while(correctBaseUnit && !correctTargetUnit){
+            System.out.println("Choose desired target unit:");
+            System.out.println("mm, cm, dm, m, km");
+
+            inputTargetUnit = scan.nextLine().toLowerCase();
+
+            if(lengthUnits.contains(inputTargetUnit)) {
+                correctTargetUnit = true;
+                System.out.println("You chose " + inputTargetUnit + ".");
+            } else {
+                inputTargetUnit = null;
+                System.out.println("You chose wrong target unit. Try again.");
+            }
+        }
+
+        //jezeli wybrano juz bazowa i docelowa jednostke to nastepuje wprowadzenie liczby do konwersji. petla powtarzana tak dlugo, az liczba bedzie poprawna
+        while(correctTargetUnit & !correctInputValue){
+            System.out.println("Enter value you want to convert [" + inputBaseUnit +"]");
+
+            //TODO Dodanie sprawdzenia poprawności wprowadzonej wartości. Co jeżeli ktoś wpisze znaki, których nie będziemy w stanie konwertować do double inputValue?
+            inputValue = scan.nextDouble();
+            correctInputValue = true;
+
+            if(correctInputValue) {
+                //wywoluje konwersje zaleznie od podanych jednostek i wartosci bazowej
+                if (inputTargetUnit.equals("mm")){
+                    outputValue = converter.toMilimeters(inputValue);
+                } else if (inputTargetUnit.equals("cm")){
+                    outputValue = converter.toCentimeters(inputValue);
+                } else if (inputTargetUnit.equals("dm")){
+                    outputValue = converter.toDecimeters(inputValue);
+                } else if (inputTargetUnit.equals("m")){
+                    outputValue = converter.toMeters(inputValue);
+                } else if (inputTargetUnit.equals("km")){
+                    outputValue = converter.toKilometers(inputValue);
+                }
+                //wysylamy wynik
+                System.out.println(inputValue + inputBaseUnit + " equals to " + outputValue + inputTargetUnit + ".");
 
             }
 
-            break;
         }
     }
+
+
 }
--- src/LengthUnit.java
+++ src/LengthUnit.java
@@ -41,3 +41,115 @@ class Milimeter extends LengthUnit{
         return input * toKilometersRatio;
     }
  }
+
+class Centimeter extends LengthUnit{
+    double toMilimetersRatio = 10f;
+    double toCentimetersRatio = 1f;
+    double toDecimetersRatio = 0.1f;
+    double toMetersRatio = 0.01f;
+    double toKilometersRatio = 0.00001f;
+
+    double toMilimeters(double input){
+        return input * toMilimetersRatio;
+    }
+
+    double toCentimeters(double input){
+        return input * toCentimetersRatio;
+    }
+
+    double toDecimeters(double input){
+        return input * toDecimetersRatio;
+    }
+
+    double toMeters(double input){
+        return input * toMetersRatio;
+    }
+
+    double toKilometers(double input){
+        return input * toKilometersRatio;
+    }
+}
+
+class Decimeter extends LengthUnit{
+    double toMilimetersRatio = 100f;
+    double toCentimetersRatio = 10f;
+    double toDecimetersRatio = 1f;
+    double toMetersRatio = 0.1f;
+    double toKilometersRatio = 0.0001f;
+
+    double toMilimeters(double input){
+        return input * toMilimetersRatio;
+    }
+
+    double toCentimeters(double input){
+        return input * toCentimetersRatio;
+    }
+
+    double toDecimeters(double input){
+        return input * toDecimetersRatio;
+    }
+
+    double toMeters(double input){
+        return input * toMetersRatio;
+    }
+
+    double toKilometers(double input){
+        return input * toKilometersRatio;
+    }
+}
+
+class Meter extends LengthUnit{
+    double toMilimetersRatio = 0.001f;
+    double toCentimetersRatio = 0.01f;
+    double toDecimetersRatio = 0.1f;
+    double toMetersRatio = 1f;
+    double toKilometersRatio = 0.001f;
+
+    double toMilimeters(double input){
+        return input * toMilimetersRatio;
+    }
+
+    double toCentimeters(double input){
+        return input * toCentimetersRatio;
+    }
+
+    double toDecimeters(double input){
+        return input * toDecimetersRatio;
+    }
+
+    double toMeters(double input){
+        return input * toMetersRatio;
+    }
+
+    double toKilometers(double input){
+        return input * toKilometersRatio;
+    }
+}
+
+class Kilometer extends LengthUnit{
+    double toMilimetersRatio = 1000000f;
+    double toCentimetersRatio = 100000f;
+    double toDecimetersRatio = 10000f;
+    double toMetersRatio = 1000f;
+    double toKilometersRatio = 1f;
+
+    double toMilimeters(double input){
+        return input * toMilimetersRatio;
+    }
+
+    double toCentimeters(double input){
+        return input * toCentimetersRatio;
+    }
+
+    double toDecimeters(double input){
+        return input * toDecimetersRatio;
+    }
+
+    double toMeters(double input){
+        return input * toMetersRatio;
+    }
+
+    double toKilometers(double input){
+        return input * toKilometersRatio;
+    }
+}
